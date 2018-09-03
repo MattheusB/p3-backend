3776eb0387768f8ba6907f5a6d1dfe5250ac1b01
MattheusB <mattheus.rodrigues@ccc.ufcg.edu.br>
2018-09-03 15:53:15 -0300

    Parte 01 do Roteiro 02 finalizada

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.use(express.json());
 const users = [
     {id: 1, name: "usuario1", email: "usuario1.gmail.com", foto: "foto1", informacoes: "Pug muito lindo etc"},
     {id: 2, name: "usuario2", email: "usuario2.gmail.com", foto: "foto2", informacoes: "Bulldog muito feio etc"},
-    {id: 1, name: "usuario3", email: "usuario3.gmail.com", foto: "foto3", informacoes: "Husky meio lindo etc"},
+    {id: 3, name: "usuario3", email: "usuario3.gmail.com", foto: "foto3", informacoes: "Husky meio lindo etc"},
 ];
 
 const chats = [
@@ -54,9 +54,59 @@ app.get("/chat/:id", (req,res) =>{
     }
 });
 
-app.post(){
-    
-}
+app.post("/user", (req,res) =>{
+    const user = {
+        "id": users.length + 1,
+        "name": req.body.name,
+        "email": req.body.email,
+        "informacoes": req.body.informacoes
+    };
+    users.push(user);
+    res.send("Usuário cadastrado com sucesso");
+});
+
+app.put("/user/:id", (req,res) =>{
+    const user = findUser(req.params.id);
+    if(!user){
+        res.status(404).send("Usuário com esse id não foi encontrado");
+    }else{
+        user.name = req.body.name ||  user.name
+        user.email = req.body.email || user.email
+        user.informacoes = req.body.informacoes || user.informacoes
+    }
+});
+
+app.put("/chat/:id", (req,res) =>{
+    const chat = findChat(req.params.id);
+    if(!chat){
+        res.status(404).res.send("Chat com esse id não foi encontrado");
+    }else {
+        chat.user1 = req.body.user1 || chat.user1,
+        chat.user2 = req.body.user2 || chat.user2
+    }
+});
+
+app.delete("/user/:id", (req, res) =>{
+    const user = findUser(req.params.id);
+
+    if(user){
+        const index = users.indexOf(user);
+        users.splice(index,1);
+    }else{
+        res.status(404).send("Usuário com esse id não foi encontrado");
+    }
+});
+
+app.delete("/chat/:id", (req,res) =>{
+    const chat = findChat(req.params.id);
+    if(chat){
+        const index = chats.indexOf(chat);
+        chats.splice(index,1);
+    }else{
+        res.status(404).send("Chat com esse id não foi encontrado");
+    }
+});
+
 
 function findUser(userId){
     const user = users.find((user) => user.id === parseInt(userId));
